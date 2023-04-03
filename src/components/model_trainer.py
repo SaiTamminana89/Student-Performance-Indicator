5289c70f659d021d330e3804b40b64e42a3ddb5a
SaiTamminana89 <saitamminana@gmail.com>
2023-04-03 11:59:33 +0530

    hyperparamter tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -46,9 +46,46 @@ class ModelTrainer:
                 "CatBoosting Regressor": CatBoostRegressor(verbose=False),
                 "AdaBoost Regressor": AdaBoostRegressor(),
             }
+            parameters = {
+                "Decision Tree": {
+                    'criterion':['squared_error','friedman_mse','absolute_error','poisson']
+                },
+                "Random Forest":{
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                
+                "Gradient Boosting":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    'n_estimators':[8,16,32,64,128,256]
+
+                },
+                
+                "Linear Regression":
+                    {},
+                    "K-Neighbors Regressor":
+                        {'n_neighbors':[5,7,9,11]
+                         },
+                        
+                   "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                     "CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                   "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                
+            }
            
             model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
-                                             models=models)
+                                             models=models,params=parameters)
             
             ## To get best model score from dict
             best_model_score = max(sorted(model_report.values()))
--- src/utils.py
+++ src/utils.py
@@ -22,14 +22,19 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_models(X_train, y_train,X_test,y_test,models):
+def evaluate_models(X_train, y_train,X_test,y_test,models,params):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
-
+            para =params[list(models.keys())[i]]
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train) 
+            
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
+            
 
             #model.fit(X_train, y_train)  # Train model
 
